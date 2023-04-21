c728cc504b6bc9008e089d9091d9d34b408e1bb7
Zlobryak <clychnik@gmail.com>
2023-04-21 22:18:37 +0300

    Fully Qualified Name

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,10 @@
 package ru.netology.service;
-
-import org.testng.annotations.Test;
-
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.testng.annotations.Test
     public void testRemain() {
         int actual = service.remain(1300);
         int expected = 700;
