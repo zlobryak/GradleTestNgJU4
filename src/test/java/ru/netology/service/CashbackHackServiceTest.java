4c8a095ba0217397ae5e1236481bee1d0f822eef
Neto Homework <clychnik@gmail.com>
2023-04-24 00:07:05 +0300

    Added TestNg tests.

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,10 +5,38 @@ public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @org.testng.annotations.Test
-    public void testRemain() {
-        int actual = service.remain(1300);
-        int expected = 700;
+    public void testRemainFor1001() {
+        int actual = service.remain(1001);
+        int expected = 999;
         assertEquals(actual, expected);
 
     }
+    @org.testng.annotations.Test
+    public void testRemainFor1() {
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(actual, expected);
+
+    }
+    @org.testng.annotations.Test
+    public void testRemainFor999() {
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+
+    }
+    @org.testng.annotations.Test
+    public void testRemainFor1000() {
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }    @org.testng.annotations.Test
+    public void testRemainFro0() {
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
 }
