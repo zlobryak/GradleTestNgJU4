864aaf7fca782b803d0c15d0a645eb2123fae2b5
Zlobryak <clychnik@gmail.com>
2023-04-24 16:45:26 +0300

    Added Jupiter.api test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,22 +4,55 @@ package ru.netology.service;
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
+  CashbackHackService service = new CashbackHackService();
 
-    @org.junit.Test
-    public void remainFor1() {
-        assertEquals(999, service.remain(1));
-    }
-    @org.junit.Test
-    public void remainFor0() {
-        assertEquals(1000, service.remain(0));
-    }
-    @org.junit.Test
-    public void remainFor1001() {
-        assertEquals(999, service.remain(1001));
-    }
-    @org.junit.Test
-    public void remainFor1000() {
-        assertEquals(0, service.remain(1000));
-    }
+  @org.junit.Test
+  public void remainFor1() {
+    assertEquals(999, service.remain(1));
+  }
+  @org.junit.Test
+  public void remainFor999() {
+    assertEquals(1, service.remain(999));
+  }
+
+  @org.junit.Test
+  public void remainFor0() {
+    assertEquals(1000, service.remain(0));
+  }
+
+  @org.junit.Test
+  public void remainFor1001() {
+    assertEquals(999, service.remain(1001));
+  }
+
+  @org.junit.Test
+  public void remainFor1000() {
+    assertEquals(0, service.remain(1000));
+  }
+
+  @org.junit.jupiter.api.Test
+  public void remainFor1Jupiter() {
+    assertEquals(999, service.remain(1));
+  }
+
+  @org.junit.jupiter.api.Test
+  public void remainFor0Jupiter() {
+    assertEquals(1000, service.remain(0));
+  }
+
+  @org.junit.jupiter.api.Test
+  public void remainFor1001Jupiter() {
+    assertEquals(999, service.remain(1001));
+  }
+
+
+  @org.junit.jupiter.api.Test
+  public void remainFor1000Jupiter() {
+    assertEquals(0, service.remain(1000));
+  }
+
+  @org.junit.jupiter.api.Test
+  public void remainFor999Jupiter() {
+    assertEquals(1, service.remain(999));
+  }
 }
